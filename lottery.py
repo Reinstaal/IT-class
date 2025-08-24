3d5473a65bb8adddc511f8d420bec6c6d7efd4f7
Reinstaal <git_topsakal@web.de>
2025-08-24 19:33:48 +0200

    actually liked the function more

--- lottery.py
+++ lottery.py
@@ -1,6 +1,17 @@
 import random
 from time import perf_counter
 
+def draw():
+    drawnNumbers = []
+    # while len(drewNumbers) by absolutezero #IStoleCode #Thanks
+    # hard coded variants: 3 or 6
+    # while len(drawnNumbers) < 3:
+    while len(drawnNumbers) < 6:
+        drawingNumber = random.randrange(1, 50)
+        if drawingNumber not in drawnNumbers:
+            drawnNumbers.append(drawingNumber)
+    return drawnNumbers
+
 def tippCheck(tipp_to_check, lastDrawn): 
     return (tipp_to_check == lastDrawn)
 
@@ -8,26 +19,16 @@ def main():
     runs = 0
     print("Gambling..")
     # Use instead if maximum amount of loops preferred
-    # for i in range(1000000000):
+    # for i in range(10000000):
     while True:
         runs += 1
-        drewNumbers = []
-        # while len(drewNumbers) by absolutezero #IStoleCode #Thanks
-        # hard coded variants: 3 or 6
-        # while len(drewNumbers) < 3:
-        while len(drewNumbers) < 6:
-            drawingNumber = random.randrange(1, 50)
-            if drawingNumber not in drewNumbers:
-                drewNumbers.append(drawingNumber)
+        drewNumbers = draw()
         if tippCheck(tippToCheck, drewNumbers):
             print(f"Runs needed to win: {runs}")
-            timerStop = perf_counter()
-            print("Used time:", "{:.2f}".format(timerStop-timerStart), "seconds")
             break
-
+        
 
 tippToCheck = []
-drewNumbers = []
 #hard coded gamble try
 #3 or 6 variant
 tippToCheck += [1,2,3,
@@ -35,4 +36,7 @@ tippToCheck += [1,2,3,
                    4,5,6]
 timerStart = perf_counter()
 
+
 main()
+timerStop = perf_counter()
+print("Used time:", "{:.2f}".format(timerStop-timerStart), "seconds")
