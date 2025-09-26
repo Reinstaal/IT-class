68e3c3d0b40e9aa01aac9c4c92a5379f3ec8a4fc
Reinstaal <git_topsakal@web.de>
2025-09-26 08:15:40 +0200

    moving to functions and adding menu

--- 2nd-semester/can-level.py
+++ 2nd-semester/can-level.py
@@ -5,19 +5,53 @@ class Tank:
         self.capacity = capacity
 
     def showInfo(self):
-        return f"Tank: {self.name}, current Level: {self.level} out of {self.capacity}L"
+        return f"{self.name}, current Level: {self.level} out of {self.capacity}L"
 
 
-tanks = []
-
-while True:
-    print("Please put in variables for naming, level and capacity for the can. Type 'Stop' to end and show all.")
+def addTank():
+    print("Please put in variables for naming, level and capacity for the can. Type 'Stop' any time to end and show all.")
     name = input("Insert tank name: ")
     level = input("Insert current level: ")
     capacity = input("Insert maximum capacity of tank: ")
     if "stop" in (name.lower(), level.lower(), capacity.lower()):
-        break
-    tanks.append(Tank(name, level, capacity))
+        return
+    return tanks.append(Tank(name, level, capacity))
 
-for tank in tanks:
-    print(tank.showInfo())
+def printTanks():
+    print("\n------------ ------------ ------------")
+    for tank in tanks:
+        print(tank.showInfo())
+    print("------------ ------------ ------------\n")
+
+def quitApp():
+    print("Roger that, quitting! T(h)anks for using!")
+    raise QuitApp
+
+def menu():
+    try:
+        print("Welcome to your totally trusted can level app.")
+        while True:
+            menuChoice = input("Input and enter one of the following to continue, or press 'Enter' to close the app: \n"
+            "1 - Add tank \n"
+            "2 - Show tanks \n"
+            "3 - Exit \n")
+            match menuChoice:
+                case "":
+                    quitApp()
+                case "3":
+                    quitApp()
+                case "1":
+                    addTank()
+                case "2":
+                    printTanks()
+                case _:
+                    print("Unaccapted input.\n")
+    except QuitApp:
+        pass
+
+class QuitApp(Exception):
+    pass
+
+
+tanks = []
+menu()
